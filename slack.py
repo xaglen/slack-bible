7c1d9729258c2d89e986342f56ad87b258c3d758
xaglen <glen.davis@gmail.com>
2023-06-05 06:34:07 -0700

    typo

--- slack.py
+++ slack.py
@@ -83,7 +83,9 @@ with open('/www/vhosts/xastanford.org/wsgi/xadb/scripts/bible/wisdom.csv', newli
 #wisdom_chapters=sum(wisdom_books.values())
 
 wisdom_progress=(weeks*5+day_of_week) % wisdom_entries
+print("Wisdom progress "+wisdom_progress)
 wisdom_passage=wisdom[wisdom_progress]
+print("Wisdom passage "+wisdom_passage)
 wisdom_passage_string = "Wisdom reading: <http://www.biblegateway.com/passage/?search={}&version=NIV'|{}>".format(urllib.parse.quote(wisdom_passage),wisdom_passage)
 
 #if progress<=150:
